889405d1e8fd5cee08d968670102081392edb212
userBrenda <102670267+userBrenda@users.noreply.github.com>
2022-04-01 16:50:26 +0300

    Add files via upload

---
+++ productof twointegers.c
@@ -0,0 +1,23 @@
+//a program to get the product of two integers
+#include<stdio.h>
+int main()
+{
+int num1;
+int num2;
+printf("enter first number");
+scanf("%d",& num1);
+printf("enter second number");
+scanf("%d",& num2);
+int product = num1*num2;
+printf("The product is :%d", product);
+
+
+
+return 0;
+
+
+
+
+}
+
+
